874437fb2c15e945e922b995234851d00137ea1e
erika cueva <ecueva88@gmail.com>
2021-01-16 14:03:11 -0800

    added vendor_info.js file

---
+++ models/vendor_info.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, DataTypes) {
+    const vendorInfo = sequelize.define("vendorInfo", {
+                vendorName: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    validate: {
+                        len: [1]
+                    }
+                },
+                service: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    validate: {
+                        len: [1]
+                    }
+                },
+                email: {
+                    type: DataTypes.STRING,
+                    allowNull: true,
+                    validate: {
+                        isEmail: true
+                    }
+                },
+                phoneNumber: {
+                    type: DataTypes.STRING,
+                    allowNull: true,
+                    validate: {
+                        isEmail: true
+                    }
+                },
+            })
+        };
