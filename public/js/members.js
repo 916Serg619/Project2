fe8e4d8f7294b31e94c11afd5bb60902f7c96600
Brian Parker <btparker70@gmail.com>
2021-01-18 21:45:35 -0800

    added onclicks for members.js. made edits to members.html

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,15 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+
+  // This takes the value from the drop down
+  // and does a page redirect when the select wedding button is clicked to that page
+  $("#selectWedding").on("click", () => {
+    const getValue = $("#weddingList").val();
+    window.location.href = `/${getValue}`;
+  });
+
+  $("#newWedding").on("click", () => {
+    window.location.href = "/planner";
+  });
 });
