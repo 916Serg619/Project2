77f4d50939805befb273ff88f59fdf8c7b56434c
WebmasterShane <shanelwebber@gmail.com>
2021-01-22 23:39:52 -0800

    updated with GSAP

--- public/js/planner.js
+++ public/js/planner.js
@@ -1,62 +1,62 @@
 $(document).ready(() => {
-    // vars below connect to Google API for autocomplete
-    const gpaInput = document.getElementById("venueInput");
-    // eslint-disable-next-line no-unused-vars
-    const autocomplete = new google.maps.places.Autocomplete(gpaInput);
+  // vars below connect to Google API for autocomplete
+  const gpaInput = document.getElementById("venueInput");
+  // eslint-disable-next-line no-unused-vars
+  const autocomplete = new google.maps.places.Autocomplete(gpaInput);
 
-    // Takes the data from the page and adds it to the db
-    $("#submitNewWedding").on("click", async event => {
-        event.preventDefault();
+  // Takes the data from the page and adds it to the db
+  $("#submitNewWedding").on("click", async event => {
+    event.preventDefault();
 
-        const eventData = {
-            couple: $("#coupleInput")
-                .val()
-                .trim(),
-            venue: $("#venueInput")
-                .val()
-                .trim(),
-            date: $("#dateInput")
-                .val()
-                .trim(),
-            foodOptionOne: $("#foodOptionOneInput")
-                .val()
-                .trim(),
-            foodOptionTwo: $("#foodOptionTwoInput")
-                .val()
-                .trim()
-        };
-        await enterData(eventData).then(() => {
-            window.location.href = "/members";
-        });
+    const eventData = {
+      couple: $("#coupleInput")
+        .val()
+        .trim(),
+      venue: $("#venueInput")
+        .val()
+        .trim(),
+      date: $("#dateInput")
+        .val()
+        .trim(),
+      foodOptionOne: $("#foodOptionOneInput")
+        .val()
+        .trim(),
+      foodOptionTwo: $("#foodOptionTwoInput")
+        .val()
+        .trim()
+    };
+    await enterData(eventData).then(() => {
+      window.location.href = "/members";
     });
+  });
 
-    // Clears the text areas
-    $("#submitNewWedding").bind("click", () => {
-        $("input[type=text], textarea").val("");
-    });
+  // Clears the text areas
+  $("#submitNewWedding").bind("click", () => {
+    $("input[type=text], textarea").val("");
+  });
 
-    // Create new entry in db
-    function enterData(eventData) {
-        return $.post("/api/eventInfos", {
-                couple: eventData.couple,
-                venueName: eventData.venue,
-                eventDate: eventData.date,
-                foodOptionOne: eventData.foodOptionOne,
-                foodOptionTwo: eventData.foodOptionTwo
-            })
-            .then(() => {
-                console.log(eventData);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-    }
+  // Create new entry in db
+  function enterData(eventData) {
+    return $.post("/api/eventInfos", {
+      couple: eventData.couple,
+      venueName: eventData.venue,
+      eventDate: eventData.date,
+      foodOptionOne: eventData.foodOptionOne,
+      foodOptionTwo: eventData.foodOptionTwo
+    })
+      .then(() => {
+        console.log(eventData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    // Navigation buttons
-    $("#backToMain").on("click", () => {
-        window.location.href = "/members";
-    });
-    $("#toSummary").on("click", () => {
-        window.location.href = "/summary";
-    });
+  // Navigation buttons
+  $("#backToMain").on("click", () => {
+    window.location.href = "/members";
+  });
+  $("#toSummary").on("click", () => {
+    window.location.href = "/summary";
+  });
 });
--- public/js/summary.js
+++ public/js/summary.js
@@ -1,43 +1,43 @@
 $(document).ready(() => {
-    // Edit button for event info
-    $("#editEvent").on("click", () => {
-        window.location.href = "/planner";
+  // Edit button for event info
+  $("#editEvent").on("click", () => {
+    window.location.href = "/planner";
+  });
+  // Delete vendor
+  $(".delete-vendor").on("click", deleteVendor()); {
+  {
+    console.log("this was clicked"), preventDefault();
+  }
+
+  function deleteVendor() {
+    console.log();
+    console.log("this was deleted");
+    const data1 = $(this).attr("data-id");
+    console.log(data1);
+    $.ajax({
+      method: "DELETE",
+      url: "/api/vendorInfos/" + data
     });
-    // Delete vendor
-    $(".delete-vendor").on("click", deleteVendor()); {
-        console.log("this was clicked"), preventDefault();
-    }
+  }
+  // Delete event
+  $(".delete-event").on("click", deleteEvent());
 
-    function deleteVendor() {
-        console.log();
-        console.log("this was deleted");
-        const data1 = $(this).attr("data-id");
-        console.log(data1);
-        $.ajax({
-            method: "DELETE",
-            url: "/api/vendorInfos/" + data
-        });
-    }
-    // Delete event
-    $(".delete-event").on("click", deleteEvent());
-
-    function deleteEvent() {
-        console.log("this was deleted");
-        const data = $(this).attr("data-id");
-        console.log(data);
-        $.ajax({
-            method: "DELETE",
-            url: "/api/eventInfos/" + data
-        });
-    }
-    //GSAP//
-    var tl = gsap.timeline({ defaults: { duration: 0.5 } })
+  function deleteEvent() {
+    console.log("this was deleted");
+    const data = $(this).attr("data-id");
+    console.log(data);
+    $.ajax({
+      method: "DELETE",
+      url: "/api/eventInfos/" + data
+    });
+  }
+  //GSAP//
+  var tl = gsap.timeline({ defaults: { duration: 0.5 } });
 
 
-
-    tl.from(".card-title", { y: -50, stagger: 0.2, opacity: 0 })
-        .from(".form-group", { y: -50, stagger: 0.2, opacity: 0 })
-        .from(".btn", { y: -50, opacity: 0, }, )
-        .from(".newSign", { y: -50, opacity: 0 })
-        .from(".navbar", { x: -100, stagger: 0.2, opacity: 0 })
+  tl.from(".card-title", { y: -50, stagger: 0.2, opacity: 0 })
+    .from(".form-group", { y: -50, stagger: 0.2, opacity: 0 })
+    .from(".btn", { y: -50, opacity: 0, }, )
+    .from(".newSign", { y: -50, opacity: 0 })
+    .from(".navbar", { x: -100, stagger: 0.2, opacity: 0 });
 });
